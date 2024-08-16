3d15fa4c0805b0048859f349adc61f8891b6a026
abdoulfataho <yamyaogofataho@gmail.com>
2024-08-16 11:06:48 -0400

    Glab 309.4-SP6

---
+++ src/main/java/com/example/SpringFormsandDataBinding/SpringFormsandDataBindingApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringFormsandDataBinding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFormsandDataBindingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFormsandDataBindingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringFormsandDataBinding/SpringFormsandDataBindingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringFormsandDataBinding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFormsandDataBindingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
